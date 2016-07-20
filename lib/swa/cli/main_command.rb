da0fce195c62ba6d47a114aff96e7ff5e2c19134
Mike Williams <mdub@dogbiscuit.org>
2016-07-20 22:52:45 +1000

    Go even shorter.

--- lib/swa/cli/main_command.rb
+++ lib/swa/cli/main_command.rb
@@ -12,6 +12,26 @@ module Swa
       subcommand "iam", "IAM stuff", IamCommand
       subcommand "kms", "KMS stuff", KmsCommand
 
+      protected
+
+      RESOURCE_PREFIXES_BY_SERVICE = {
+        "ec2" => %w(ami i sg subnet vpc)
+      }
+
+      def subcommand_for_prefix(prefix)
+        RESOURCE_PREFIXES_BY_SERVICE.each do |subcommand, prefixes|
+          return subcommand if prefixes.member?(prefix)
+        end
+      end
+
+      def parse(arguments)
+        if arguments.first =~ /^(\w+)-/
+          subcommand = subcommand_for_prefix($1)
+          arguments = [subcommand] + arguments if subcommand
+        end
+        super(arguments)
+      end
+
     end
 
   end
